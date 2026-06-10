d1b45d14ef6ec5bafd2fbe5c912b3f0fd37cad8f
piter <peterpacka15@gmail.com>
2026-06-10 16:12:48 +0200

    feat(projects): added new project MM

--- src/i18n/translation.ts
+++ src/i18n/translation.ts
@@ -25,7 +25,16 @@ export const translation = {
         instagram: "ten_peter_",
       },
     ],
-    projects: [],
+    projects: [
+      {
+        slug: "modranski-motoriste",
+        images: ["/projects/mm.png"],
+        name: "Modranskí Motoristé",
+        description:
+          "Modranskí motoristi je študentský projekt z Gymnázia Karola Štúra v Modre, v rámci ktorého žiaci navrhujú a stavajú vlastnú funkčnú formulu.",
+        website: "https://mm.gksm.sk/",
+      },
+    ],
   },
   en: {
     nav: {
@@ -53,6 +62,15 @@ export const translation = {
         instagram: "ten_peter_",
       },
     ],
-    projects: [],
+    projects: [
+      {
+        slug: "modranski-motoriste",
+        images: ["/projects/mm.png"],
+        name: "Modranskí Motoristé",
+        description:
+          "Modranskí motoristi is a student project from the Karol Štúr Grammar School in Modra, where students design and build their own functional formula racing car.",
+        website: "https://mm.gksm.sk/",
+      },
+    ],
   },
 };
